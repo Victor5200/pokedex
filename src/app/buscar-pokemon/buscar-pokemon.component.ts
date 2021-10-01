d9155cee64d80418993a6171417949178fdc3b0e
Victor Alex <victoralexsander5200@gmail.com>
2021-10-01 20:03:04 -0300

    implementando busca por pokemons

--- src/app/buscar-pokemon/buscar-pokemon.component.ts
+++ src/app/buscar-pokemon/buscar-pokemon.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PokemonApiService} from '../service/pokemon-api-service';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-buscar-pokemon',
@@ -8,14 +9,21 @@ import {PokemonApiService} from '../service/pokemon-api-service';
 })
 export class BuscarPokemonComponent implements OnInit {
   pokemons: any;
+  formulario: FormGroup;
 
-  constructor(private pokemonApiService: PokemonApiService) { }
+  constructor(private pokemonApiService: PokemonApiService,
+              private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      nome: []
+    });
+  }
 
   buscarPokemon(): void {
-    this.pokemonApiService.searchAllPokemon().subscribe(resp => {
-      this.pokemons = resp.results;
+    const formularioObjeto = this.formulario.value;
+    this.pokemonApiService.searchPokemonByName(formularioObjeto.nome).subscribe(resp => {
+      this.pokemons = resp;
     });
   }
 }
